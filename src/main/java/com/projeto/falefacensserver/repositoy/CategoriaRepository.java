1f0401b83e8d9292862c10903d73f2f64afd5688
Thirbt <thiago.godo@hotmail.com>
2023-09-10 04:04:30 -0300

    Adicionando querys de busca por atributo dentro dos repositórios

--- src/main/java/com/projeto/falefacensserver/repositoy/CategoriaRepository.java
+++ src/main/java/com/projeto/falefacensserver/repositoy/CategoriaRepository.java
@@ -2,6 +2,13 @@ package com.projeto.falefacensserver.repositoy;
 
 import com.projeto.falefacensserver.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    @Query(value = "SELECT u FROM Categoria u WHERE nome = :nome")
+    Optional<Categoria> findyByNome(@Param("nome") String nome);
 }
--- src/main/java/com/projeto/falefacensserver/repositoy/ContatoRepositoy.java
+++ src/main/java/com/projeto/falefacensserver/repositoy/ContatoRepositoy.java
@@ -2,6 +2,19 @@ package com.projeto.falefacensserver.repositoy;
 
 import com.projeto.falefacensserver.model.Contato;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ContatoRepositoy extends JpaRepository<Contato, Long> {
+
+    @Query(value = "SELECT u FROM Contato u WHERE nome = :nome")
+    Optional<Contato> findyByNome(@Param("nome") String nome);
+
+    @Query(value = "SELECT u FROM Contato u WHERE email = :email")
+    Optional<Contato> findyByEmail(@Param("email") String email);
+
+    @Query(value = "SELECT u FROM Contato u WHERE telefone = :telefone")
+    Optional<Contato> findyByTelefone(@Param("telefone") String telefone);
 }
